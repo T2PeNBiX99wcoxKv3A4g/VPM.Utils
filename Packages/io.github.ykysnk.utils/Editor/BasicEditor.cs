33627216df6405671241ce7f200c85dfc722780b
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-11-17 19:29:41 +0800

    refactor: Bump package version to 0.2.7 and add BasicEditor class for custom inspector support in Unity

---
+++ Packages/io.github.ykysnk.utils/Editor/BasicEditor.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace io.github.ykysnk.utils.Editor;
+
+public abstract class BasicEditor : UnityEditor.Editor
+{
+    protected virtual void OnEnable()
+    {
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUI.BeginChangeCheck();
+
+        try
+        {
+            OnInspectorGUIDraw();
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+            EditorGUILayout.HelpBox($"Editor Error: {e.Message}\n{e.StackTrace}", MessageType.Error, true);
+        }
+
+        if (EditorGUI.EndChangeCheck())
+            serializedObject.ApplyModifiedProperties();
+    }
+
+    protected virtual void OnInspectorGUIDraw()
+    {
+    }
+}
