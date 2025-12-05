389ede4f1534410300755a2aed2f80adbeb736a5
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-12-06 02:03:05 +0800

    feat: Add new `Multiply` and `Round` methods to `Vector3Extensions`

--- Packages/io.github.ykysnk.utils/Runtime/Extensions/Vector3Extensions.cs
+++ Packages/io.github.ykysnk.utils/Runtime/Extensions/Vector3Extensions.cs
@@ -1,3 +1,4 @@
+using System;
 using JetBrains.Annotations;
 using UnityEngine;
 
@@ -20,5 +21,16 @@ namespace io.github.ykysnk.utils.Extensions
         public static float Distance2D(this Vector3 a, [NotNull] Transform b) => a.Distance2D(b.position);
 
         public static Vector3 Divide(this Vector3 a, Vector3 b) => new Vector3(a.x / b.x, a.y / b.y, a.z / b.z);
+
+        public static Vector3 Multiply(this Vector3 a, Vector3 b) => new Vector3(a.x * b.x, a.y * b.y, a.z * b.z);
+
+        public static Vector3 Round(this Vector3 a, int digits, MidpointRounding mode) =>
+            new Vector3(MathF.Round(a.x, digits, mode), MathF.Round(a.y, digits, mode), MathF.Round(a.z, digits, mode));
+
+        public static Vector3 Round(this Vector3 a, int digits) => a.Round(digits, MidpointRounding.ToEven);
+
+        public static Vector3 Round(this Vector3 a, MidpointRounding mode) => a.Round(0, mode);
+
+        public static Vector3 Round(this Vector3 a) => a.Round(0);
     }
 }
