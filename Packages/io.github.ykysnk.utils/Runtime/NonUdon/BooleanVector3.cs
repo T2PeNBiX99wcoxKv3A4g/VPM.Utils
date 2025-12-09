61318850f2d8d59c22be1adc6f794126c48f2f10
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-12-09 20:56:40 +0800

    refactor: Apply file-scoped namespaces and update C# language version to 10

--- Packages/io.github.ykysnk.utils/Runtime/NonUdon/BooleanVector3.cs
+++ Packages/io.github.ykysnk.utils/Runtime/NonUdon/BooleanVector3.cs
@@ -2,97 +2,96 @@ using System;
 using System.Linq;
 using JetBrains.Annotations;
 
-namespace io.github.ykysnk.utils.NonUdon
+namespace io.github.ykysnk.utils.NonUdon;
+
+[Serializable]
+[PublicAPI]
+public struct BooleanVector3
 {
-    [Serializable]
-    [PublicAPI]
-    public struct BooleanVector3
+    public bool x;
+    public bool y;
+    public bool z;
+
+    public int Flags
     {
-        public bool x;
-        public bool y;
-        public bool z;
-
-        public int Flags
+        get => (x ? (int)Flag.X : 0) | (y ? (int)Flag.Y : 0) | (z ? (int)Flag.Z : 0);
+        set
         {
-            get => (x ? (int)Flag.X : 0) | (y ? (int)Flag.Y : 0) | (z ? (int)Flag.Z : 0);
-            set
-            {
-                x = (value & (int)Flag.X) != 0;
-                y = (value & (int)Flag.Y) != 0;
-                z = (value & (int)Flag.Z) != 0;
-            }
+            x = (value & (int)Flag.X) != 0;
+            y = (value & (int)Flag.Y) != 0;
+            z = (value & (int)Flag.Z) != 0;
         }
+    }
 
-        public bool HasFlag(Flag flag) => (Flags & (int)flag) == (int)flag;
+    public bool HasFlag(Flag flag) => (Flags & (int)flag) == (int)flag;
 
-        public bool HasFlags(params Flag[] flags)
-        {
-            var allFlags = flags.Aggregate(0, (current, flag) => current | (int)flag);
-            return (Flags & allFlags) == allFlags;
-        }
+    public bool HasFlags(params Flag[] flags)
+    {
+        var allFlags = flags.Aggregate(0, (current, flag) => current | (int)flag);
+        return (Flags & allFlags) == allFlags;
+    }
 
-        public void AddFlag(Flag flag) => Flags |= (int)flag;
-        public void AddFlags(params Flag[] flags) => Flags |= flags.Aggregate(0, (current, flag) => current | (int)flag);
+    public void AddFlag(Flag flag) => Flags |= (int)flag;
+    public void AddFlags(params Flag[] flags) => Flags |= flags.Aggregate(0, (current, flag) => current | (int)flag);
 
-        public void RemoveFlag(Flag flag) => Flags &= ~(int)flag;
+    public void RemoveFlag(Flag flag) => Flags &= ~(int)flag;
 
-        public void RemoveFlags(params Flag[] flags) =>
-            Flags &= ~flags.Aggregate(0, (current, flag) => current | (int)flag);
+    public void RemoveFlags(params Flag[] flags) =>
+        Flags &= ~flags.Aggregate(0, (current, flag) => current | (int)flag);
 
-        public void SetFlag(Flag flag) => Flags = (int)flag;
-        public void SetFlags(params Flag[] flags) => Flags = flags.Aggregate(0, (current, flag) => current | (int)flag);
+    public void SetFlag(Flag flag) => Flags = (int)flag;
+    public void SetFlags(params Flag[] flags) => Flags = flags.Aggregate(0, (current, flag) => current | (int)flag);
 
-        public void Set(bool x2, bool y2, bool z2)
-        {
-            x = x2;
-            y = y2;
-            z = z2;
-        }
+    public void Set(bool x2, bool y2, bool z2)
+    {
+        x = x2;
+        y = y2;
+        z = z2;
+    }
 
-        public void Set(BooleanVector3 other) => Set(other.x, other.y, other.z);
+    public void Set(BooleanVector3 other) => Set(other.x, other.y, other.z);
 
-        public void SetAll(bool value) => Set(value, value, value);
+    public void SetAll(bool value) => Set(value, value, value);
 
-        public void Invert() => Set(!x, !y, !z);
+    public void Invert() => Set(!x, !y, !z);
 
-        public void SetX(bool value) => x = value;
-        public void SetY(bool value) => y = value;
-        public void SetZ(bool value) => z = value;
+    public void SetX(bool value) => x = value;
+    public void SetY(bool value) => y = value;
+    public void SetZ(bool value) => z = value;
 
-        public void SetTrue() => SetAll(true);
-        public void SetFalse() => SetAll(false);
+    public void SetTrue() => SetAll(true);
+    public void SetFalse() => SetAll(false);
 
-        public override string ToString() => $"BooleanVector3(x:{x},y:{y},z:{z})";
+    public override string ToString() => $"BooleanVector3(x:{x},y:{y},z:{z})";
 
-        public static BooleanVector3 True => new()
-        {
-            x = true,
-            y = true,
-            z = true
-        };
+    public static BooleanVector3 True => new()
+    {
+        x = true,
+        y = true,
+        z = true
+    };
 
-        public static BooleanVector3 False => new();
+    public static BooleanVector3 False => new();
 
-        public static BooleanVector3 operator !(BooleanVector3 booleanVector3)
-        {
-            booleanVector3.Invert();
-            return booleanVector3;
-        }
+    public static BooleanVector3 operator !(BooleanVector3 booleanVector3)
+    {
+        booleanVector3.Invert();
+        return booleanVector3;
+    }
 
-        public static bool operator true(BooleanVector3 booleanVector3) =>
-            booleanVector3.Flags == (int)Flag.All;
+    public static bool operator true(BooleanVector3 booleanVector3) =>
+        booleanVector3.Flags == (int)Flag.All;
 
-        public static bool operator false(BooleanVector3 booleanVector3) =>
-            booleanVector3.Flags != (int)Flag.All;
+    public static bool operator false(BooleanVector3 booleanVector3) =>
+        booleanVector3.Flags != (int)Flag.All;
 
-        [PublicAPI]
-        public enum Flag
-        {
-            X = 1 << 0,
-            Y = 1 << 1,
-            Z = 1 << 2,
-            None = 0,
-            All = X | Y | Z
-        }
+    [PublicAPI]
+    public enum Flag
+    {
+        X = 1 << 0,
+        Y = 1 << 1,
+        Z = 1 << 2,
+        None = 0,
+        All = X | Y | Z
     }
 }
--- Packages/io.github.ykysnk.utils/Runtime/NonUdon/ColorHex.cs
+++ Packages/io.github.ykysnk.utils/Runtime/NonUdon/ColorHex.cs
@@ -1,35 +1,34 @@
 using System;
 using UnityEngine;
 
-namespace io.github.ykysnk.utils.NonUdon
+namespace io.github.ykysnk.utils.NonUdon;
+
+[Serializable]
+public struct ColorHex
 {
-    [Serializable]
-    public struct ColorHex
+    public Color color;
+    public string hex;
+
+    public override string ToString() => $"Color: {color}, Hex: {hex}";
+
+    public static implicit operator string(ColorHex colorHex) => colorHex.hex;
+    public static implicit operator Color(ColorHex colorHex) => colorHex.color;
+
+    public static implicit operator ColorHex(string hex)
     {
-        public Color color;
-        public string hex;
+        var getColor = ColorUtility.TryParseHtmlString(hex, out var color) ? color : Color.white;
+        var getHex = $"#{ColorUtility.ToHtmlStringRGBA(getColor)}";
 
-        public override string ToString() => $"Color: {color}, Hex: {hex}";
-
-        public static implicit operator string(ColorHex colorHex) => colorHex.hex;
-        public static implicit operator Color(ColorHex colorHex) => colorHex.color;
-
-        public static implicit operator ColorHex(string hex)
+        return new()
         {
-            var getColor = ColorUtility.TryParseHtmlString(hex, out var color) ? color : Color.white;
-            var getHex = $"#{ColorUtility.ToHtmlStringRGBA(getColor)}";
-
-            return new()
-            {
-                color = getColor,
-                hex = getHex
-            };
-        }
-
-        public static implicit operator ColorHex(Color color) => new()
-        {
-            color = color,
-            hex = $"#{ColorUtility.ToHtmlStringRGBA(color)}"
+            color = getColor,
+            hex = getHex
         };
     }
+
+    public static implicit operator ColorHex(Color color) => new()
+    {
+        color = color,
+        hex = $"#{ColorUtility.ToHtmlStringRGBA(color)}"
+    };
 }
