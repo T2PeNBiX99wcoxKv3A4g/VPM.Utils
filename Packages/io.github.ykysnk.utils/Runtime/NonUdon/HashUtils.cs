e8cc14e68ea5bc6f35ab224976928cda6c7d790d
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-12-09 20:57:48 +0800

    feat: Add `HashUtils` class for computing hashes (MD5, SHA1, SHA256, SHA512)

---
+++ Packages/io.github.ykysnk.utils/Runtime/NonUdon/HashUtils.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using JetBrains.Annotations;
+
+namespace io.github.ykysnk.utils.NonUdon;
+
+[PublicAPI]
+public static class HashUtils
+{
+    public enum HashType
+    {
+        MD5,
+        SHA1,
+        SHA256,
+        SHA512
+    }
+
+    public static string ComputeHash(string input, HashType type)
+    {
+        var bytes = Encoding.UTF8.GetBytes(input);
+        byte[] hashBytes;
+
+        switch (type)
+        {
+            case HashType.MD5:
+                using (var md5 = MD5.Create())
+                    hashBytes = md5.ComputeHash(bytes);
+                break;
+
+            case HashType.SHA1:
+                using (var sha1 = SHA1.Create())
+                    hashBytes = sha1.ComputeHash(bytes);
+                break;
+
+            case HashType.SHA256:
+                using (var sha256 = SHA256.Create())
+                    hashBytes = sha256.ComputeHash(bytes);
+                break;
+
+            case HashType.SHA512:
+                using (var sha512 = SHA512.Create())
+                    hashBytes = sha512.ComputeHash(bytes);
+                break;
+
+            default:
+                throw new ArgumentException("Unsupported hash type", nameof(type));
+        }
+
+        var sb = new StringBuilder();
+        foreach (var b in hashBytes)
+            sb.Append(b.ToString("x2"));
+        return sb.ToString();
+    }
+}
