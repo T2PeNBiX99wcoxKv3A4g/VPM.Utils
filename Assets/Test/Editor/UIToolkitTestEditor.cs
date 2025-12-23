a08028acc95e1304d4facd81a891a7a335a1b830
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-12-24 02:11:23 +0800

    fix: Simplify `CreateErrorHandleInspectorGUI` in `UIToolkitTestEditor` and update error handling

--- Assets/Test/Editor/UIToolkitTestEditor.cs
+++ Assets/Test/Editor/UIToolkitTestEditor.cs
@@ -1,3 +1,4 @@
+using System;
 using io.github.ykysnk.utils.Editor;
 using UnityEditor;
 using UnityEngine;
@@ -8,21 +9,8 @@ namespace Test.Editor
     [CustomEditor(typeof(UIToolkitTest))]
     public class UIToolkitTestEditor : BasicEditor
     {
-        [SerializeField] private StyleSheet uss;
         [SerializeField] private VisualTreeAsset uxml;
 
-        protected override VisualElement CreateErrorHandleInspectorGUI()
-        {
-            var root = new VisualElement();
-
-            var visualTree = uxml.CloneTree();
-            var errorBox = visualTree.Q<HelpBox>("errorBox");
-
-            throw new("test");
-
-            root.Add(visualTree);
-
-            return root;
-        }
+        protected override VisualElement CreateErrorHandleInspectorGUI() => throw new ArgumentOutOfRangeException();
     }
 }
