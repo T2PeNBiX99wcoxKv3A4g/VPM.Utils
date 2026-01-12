d5f585ddac9d56f3b1b4d4fc670aa1097acd9e8e
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-01-12 23:32:17 +0800

    chore: Add `TaskExtensions` utility for handling async tasks in the Unity Editor

---
+++ Packages/io.github.ykysnk.utils/Editor/Extensions/TaskExtensions.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using UnityEditor;
+
+namespace io.github.ykysnk.utils.Editor.Extensions;
+
+[PublicAPI]
+public static class TaskExtensions
+{
+    public delegate void TaskWait();
+
+    public delegate void TaskWait<in T>(T result);
+
+    public delegate void TaskWaitError(Exception? e);
+
+    public static void WaitEditor(this Task task, TaskWait onCompleted, TaskWaitError? onError = null)
+    {
+        EditorApplication.update += Waiting;
+        return;
+
+        void Waiting()
+        {
+            if (!task.IsCompleted) return;
+            EditorApplication.update -= Waiting;
+
+            if (task.IsFaulted)
+                onError?.Invoke(task.Exception);
+            else
+                onCompleted();
+        }
+    }
+
+    public static void WaitEditor<T>(this Task<T> task, TaskWait<T> onCompleted, TaskWaitError? onError = null)
+    {
+        EditorApplication.update += Waiting;
+        return;
+
+        void Waiting()
+        {
+            if (!task.IsCompleted) return;
+            EditorApplication.update -= Waiting;
+
+            if (task.IsFaulted)
+                onError?.Invoke(task.Exception);
+            else
+                onCompleted(task.Result);
+        }
+    }
+}
