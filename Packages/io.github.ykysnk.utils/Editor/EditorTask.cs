9f8d436cce13f6404c8cf38b10be8019301b4985
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-01-12 23:28:09 +0800

    chore: Add `EditorTask` utility for managing asynchronous tasks in the Unity Editor

---
+++ Packages/io.github.ykysnk.utils/Editor/EditorTask.cs
@@ -0,0 +1,55 @@
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using UnityEditor;
+
+namespace io.github.ykysnk.utils.Editor;
+
+[PublicAPI]
+public static class EditorTask
+{
+    public static Task Run(Task task)
+    {
+        var tcs = new TaskCompletionSource<bool>();
+
+        EditorApplication.update += Poll;
+
+        return tcs.Task;
+
+        void Poll()
+        {
+            if (!task.IsCompleted)
+                return;
+            EditorApplication.update -= Poll;
+
+            if (task.IsFaulted)
+                tcs.SetException(task.Exception ?? new("Unknown error"));
+            else if (task.IsCanceled)
+                tcs.SetCanceled();
+            else
+                tcs.SetResult(true);
+        }
+    }
+
+    public static Task<T> Run<T>(Task<T> task)
+    {
+        var tcs = new TaskCompletionSource<T>();
+
+        EditorApplication.update += Poll;
+
+        return tcs.Task;
+
+        void Poll()
+        {
+            if (!task.IsCompleted)
+                return;
+            EditorApplication.update -= Poll;
+
+            if (task.IsFaulted)
+                tcs.SetException(task.Exception ?? new("Unknown error"));
+            else if (task.IsCanceled)
+                tcs.SetCanceled();
+            else
+                tcs.SetResult(task.Result);
+        }
+    }
+}
