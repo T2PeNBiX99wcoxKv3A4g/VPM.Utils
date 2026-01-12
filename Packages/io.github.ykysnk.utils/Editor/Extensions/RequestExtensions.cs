5f087ceb7286cddd835e8be739b150bf07548f38
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-01-12 23:32:06 +0800

    chore: Add `RequestExtensions` utility to convert Unity Editor requests into tasks

---
+++ Packages/io.github.ykysnk.utils/Editor/Extensions/RequestExtensions.cs
@@ -0,0 +1,50 @@
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using UnityEditor;
+using UnityEditor.PackageManager.Requests;
+
+namespace io.github.ykysnk.utils.Editor.Extensions;
+
+[PublicAPI]
+public static class RequestExtensions
+{
+    public static Task<T> AsTask<T>(this T request) where T : Request
+    {
+        var tcs = new TaskCompletionSource<T>();
+
+        EditorApplication.update += Check;
+
+        return tcs.Task;
+
+        void Check()
+        {
+            if (!request.IsCompleted) return;
+            EditorApplication.update -= Check;
+            tcs.SetResult(request);
+        }
+    }
+
+    public static Task<T> AsTask<T>(this T request, CancellationToken token) where T : Request
+    {
+        var tcs = new TaskCompletionSource<T>();
+
+        EditorApplication.update += Check;
+
+        return tcs.Task;
+
+        void Check()
+        {
+            if (token.IsCancellationRequested)
+            {
+                EditorApplication.update -= Check;
+                tcs.TrySetCanceled();
+                return;
+            }
+
+            if (!request.IsCompleted) return;
+            EditorApplication.update -= Check;
+            tcs.TrySetResult(request);
+        }
+    }
+}
