cb037b7cb7bcfa2c53ed04060d566752fa13b04d
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-01-13 03:38:16 +0800

    refactor: Simplify `IconTexture` implementation in `EditorGUIUtils`

--- Packages/io.github.ykysnk.utils/Editor/EditorGUIUtils.cs
+++ Packages/io.github.ykysnk.utils/Editor/EditorGUIUtils.cs
@@ -7,10 +7,7 @@ namespace io.github.ykysnk.utils.Editor;
 [PublicAPI]
 public static class EditorGUIUtils
 {
-    public static GUIContent IconContent(string name) =>
-        EditorGUIUtility.IconContent(EditorGUIUtility.isProSkin ? "d_" + name : name);
-
     public static GUIContent IconContent(string text, string name) => new(text, IconTexture(name));
 
-    public static Texture IconTexture(string name) => IconContent(name).image;
+    public static Texture IconTexture(string name) => EditorGUIUtility.IconContent(name).image;
 }
