7ff3a84071d77d9cd189f3e01260a6e75da0d895
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-02-04 05:48:27 +0800

    feat: Add `ListWrapper` generic class for handling lists with implicit conversions

---
+++ Packages/io.github.ykysnk.utils/Runtime/NonUdon/ListWrapper.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace io.github.ykysnk.utils.NonUdon;
+
+[Serializable]
+[PublicAPI]
+public class ListWrapper<T> : IEnumerable<T>
+{
+    public List<T> items;
+
+    internal ListWrapper(IEnumerable<T> source) => items = new(source);
+
+    public int Count => items.Count;
+
+    public T this[int index] => items[index];
+
+    public IEnumerator<T> GetEnumerator() => items.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public bool Contains(T item) => items.Contains(item);
+
+    public static implicit operator List<T>(ListWrapper<T> wrapper) => wrapper.items;
+    public static implicit operator ListWrapper<T>(List<T> list) => new(list);
+}
