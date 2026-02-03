7fcc3c88ac3a4945130b6b4d2f91d4e850b8e279
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-02-04 02:50:02 +0800

    refactor: Add `[PublicAPI]` attribute to `WrapperBase` for improved code annotation and external usage

--- Packages/io.github.ykysnk.utils/Editor/WrapperBase.cs
+++ Packages/io.github.ykysnk.utils/Editor/WrapperBase.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 
 namespace io.github.ykysnk.utils.Editor
 {
     [Serializable]
+    [PublicAPI]
     public class WrapperBase<T>
     {
         public List<T> items = new();
