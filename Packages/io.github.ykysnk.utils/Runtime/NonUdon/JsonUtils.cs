2735afa3ad3af0c52e989a2036528e589ff1bea8
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-02-03 08:29:09 +0800

    feat: Extend `JsonUtils` with additional safe JSON serialization and deserialization methods

--- Packages/io.github.ykysnk.utils/Runtime/NonUdon/JsonUtils.cs
+++ Packages/io.github.ykysnk.utils/Runtime/NonUdon/JsonUtils.cs
@@ -23,4 +23,57 @@ public static class JsonUtils
             return false;
         }
     }
+
+    public static bool TryFromJson(string json, Type type, out object? result, out Exception? exception)
+    {
+        result = null;
+        exception = null;
+
+        try
+        {
+            result = JsonUtility.FromJson(json, type);
+            return true;
+        }
+        catch (Exception e)
+        {
+            exception = e;
+            return false;
+        }
+    }
+
+    public static bool TryFromJsonOverwrite(string json, object objectToOverwrite, out Exception? exception)
+    {
+        exception = null;
+
+        try
+        {
+            JsonUtility.FromJsonOverwrite(json, objectToOverwrite);
+            return true;
+        }
+        catch (Exception e)
+        {
+            exception = e;
+            return false;
+        }
+    }
+
+    public static bool TryToJson(object obj, bool prettyPrint, out string? json, out Exception? exception)
+    {
+        exception = null;
+        json = null;
+
+        try
+        {
+            json = JsonUtility.ToJson(obj, prettyPrint);
+            return true;
+        }
+        catch (Exception e)
+        {
+            exception = e;
+            return false;
+        }
+    }
+
+    public static bool TryToJson(object obj, out string? json, out Exception? exception) =>
+        TryToJson(obj, false, out json, out exception);
 }
